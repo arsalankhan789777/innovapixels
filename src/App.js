2dc3ed98fb83c2c386d655b1df27b67ba1757441
arsalankhan78677@gmail.com <you@example.com>
2023-10-26 03:41:59 +0500

    hashrouter updates

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import Work from "./Work";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";  // Changed HashRouter to BrowserRouter
+import { HashRouter as Router, Routes, Route } from "react-router-dom";  // Change here
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import './index.css';
 
 const App = () => {
   return (
-    <Router>  
+    <Router> 
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
