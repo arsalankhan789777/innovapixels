176c37de24bd1d30006c04da0e8df103bfdc8def
arsalankhan78677@gmail.com <you@example.com>
2023-11-21 13:48:29 +0500

    lk

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Navbar from "./components/Navbar"; // Assuming the Navbar component file is named Navbar.js
-import Footer from "./components/Footer";
 import Home from "./Home";
 import Work from "./Work";
 import About from "./About";
 import Services from "./Services";
 import Contact from "./Contact";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";  // Change here
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 import './index.css';
 
 const App = () => {
   return (
     <Router>
       <Header />
-      <Navbar /> {/* Render the Navbar component */}
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/work" element={<Work />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
+        <Route path="/Work" element={<Work />} />
+        <Route path="/Services" element={<Services />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
