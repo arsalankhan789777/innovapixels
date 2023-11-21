5a7c734cbc99a658b9b6350d071735b743058a79
arsalankhan78677@gmail.com <you@example.com>
2023-11-21 13:40:56 +0500

    s

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Navbar from "./components/Navbar"; // Assuming the Navbar component file is named Navbar.js
 import Footer from "./components/Footer";
 import Home from "./Home";
 import Work from "./Work";
@@ -13,6 +14,7 @@ const App = () => {
   return (
     <Router>
       <Header />
+      <Navbar /> {/* Render the Navbar component */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
