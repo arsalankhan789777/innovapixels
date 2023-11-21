8f85bf6b00debe7fd1ce7d606dd881f10ea47134
arsalankhan78677@gmail.com <you@example.com>
2023-11-21 18:45:16 +0500

    saasas

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,8 @@
+// Navbar.js
 import React, { useRef, useState } from 'react'
 import { NavLink } from 'react-router-dom'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons';
-
-
 
 const Navbar = () => {
     const navRef = useRef();
@@ -38,23 +37,17 @@ const Navbar = () => {
                         <li>
                             <NavLink to="/About" activeClassName="active-link" onClick={closeNavbar}>About</NavLink>
                         </li>
-                     
                         <li>
                             <NavLink to="/Contact" activeClassName="active-link" onClick={closeNavbar}>Contact</NavLink>
                         </li>
-               
-                   
-
                     </ul>
                 </div>
-
             </nav>
             <div className={`burger-btn ${menuOpen ? "active" : ""}`} onClick={toggleNavbar}>
                 <FontAwesomeIcon icon={faBars} />
             </div>
         </div>
-
     );
 };
 
-export default Navbar
+export default Navbar;
