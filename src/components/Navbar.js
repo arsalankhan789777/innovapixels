d148a694fe5ce987b7a3f6d24b320ae8d1779b44
arsalankhan78677@gmail.com <you@example.com>
2023-11-26 00:06:19 +0500

    sa

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ const Navbar = () => {
                             <NavLink exact to="/" activeClassName="active-link" onClick={closeNavbar}>Home</NavLink>
                         </li>
                         <li>
-                            <NavLink to="/Work" activeClassName="active-link" onClick={closeNavbar}>Work22</NavLink>
+                            <NavLink to="/Work" activeClassName="active-link" onClick={closeNavbar}>Work</NavLink>
                         </li>
                         <li>
                             <NavLink to="/Services" activeClassName="active-link" onClick={closeNavbar}>Services</NavLink>
