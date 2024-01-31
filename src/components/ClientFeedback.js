c0a45861d3ca909e1e7421c288b38aadf82530e3
arsalankhan78677@gmail.com <you@example.com>
2024-01-31 23:30:23 +0500

    done

--- src/components/ClientFeedback.js
+++ src/components/ClientFeedback.js
@@ -5,20 +5,23 @@ export const ClientFeedback = () => {
     return (
         <div className='ClientFeedback'>
             <div className="container-max">
-                <video controls width="400">
-                    <source src={videotesti} type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-                <div className='content_client'>
-                <p>
-                    Salman has been working with our team for a few months and has been absolutely incredible. We needed an experienced Shopify developer to make some customized sections within our Shopify theme and Salman has been able to bring all of our ideas to life.
-                </p>
-                <p>
-                    He is extremely communicative and accomplishes every task quickly. When asked, he has also provided some very helpful tutorial videos that we have used to train ourselves for some of the sections that we find ourselves creating multiple times.
-                </p>
-                <p>
-                    We are very lucky to have found Salman after having worked with some less reliable developers in the past. If you are looking for some help on your Shopify store I cannot recommend Salman enough
-                </p>
+                <h2>what our <strong>clients</strong> say about us:</h2>
+                <div className='flex-clients-says'>
+                    <video controls width="400">
+                        <source src={videotesti} type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                    <div className='content_client'>
+                        <p>
+                            Salman has been working with our team for a few months and has been absolutely incredible. We needed an experienced Shopify developer to make some customized sections within our Shopify theme and Salman has been able to bring all of our ideas to life.
+                        </p>
+                        <p>
+                            He is extremely communicative and accomplishes every task quickly. When asked, he has also provided some very helpful tutorial videos that we have used to train ourselves for some of the sections that we find ourselves creating multiple times.
+                        </p>
+                        <p>
+                            We are very lucky to have found Salman after having worked with some less reliable developers in the past. If you are looking for some help on your Shopify store I cannot recommend Salman enough
+                        </p>
+                    </div>
                 </div>
             </div>
         </div>
