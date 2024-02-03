5109d0328fd3a7aae7e3fb855d24db3b560881b2
arsalankhan78677@gmail.com <you@example.com>
2024-02-04 02:08:12 +0500

    sss

--- src/Work.js
+++ src/Work.js
@@ -1,8 +1,14 @@
 import React, { useState, useEffect } from 'react';
+
+// Import your image files here
 import shopify1 from './components/images/images-carousel/LiveWell.PNG';
 import shopify2 from './components/images/images-carousel/NeoCharge.PNG';
 import wordpress1 from './components/images/images-carousel/Alfan.png';
+import wordpress2 from './components/images/images-carousel/calltrackingmetrics.png';
+import wordpress3 from './components/images/images-carousel/wisebuilders.org.png';
+import wordpress4 from './components/images/images-carousel/jibble.io.png';
 import wix1 from './components/images/images-carousel/quest-films.jpg';
+import webflow1 from './components/images/images-carousel/prismaticcapital.png';
 
 function TabSet({ title, tabs, activeTab, updateActiveTab }) {
   const [isToggled] = useState(false);
@@ -33,9 +39,16 @@ function TabSet({ title, tabs, activeTab, updateActiveTab }) {
 
 function ReactTabs() {
   const [activeTabStages, setActiveTabStages] = useState(0);
+  const [projectsPerPage, setProjectsPerPage] = useState(40);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const handleActiveTabChangeStages = (id) => {
     setActiveTabStages(id);
+    setCurrentPage(1); // Reset to the first page when switching tabs
+  };
+
+  const handleShowMore = () => {
+    setProjectsPerPage((prev) => prev + 2);
   };
 
   const stages = [
@@ -53,6 +66,24 @@ function ReactTabs() {
           heading: 'Alfan Group',
           text: 'View Work',
           image: wordpress1,
+        },
+        {
+          link: 'https://www.calltrackingmetrics.com/',
+          heading: 'Call Tracking Metrics',
+          text: 'View Work',
+          image: wordpress2,
+        },
+        {
+          link: 'https://wisebuilders.org/',
+          heading: 'Wise Builders',
+          text: 'View Work',
+          image: wordpress3,
+        },
+        {
+          link: 'https://www.jibble.io/',
+          heading: 'Jibble',
+          text: 'View Work',
+          image: wordpress4,
         },
        
       ],
@@ -88,16 +119,32 @@ function ReactTabs() {
         },
       ],
     },
+    {
+      id: 4,
+      label: 'Webflow',
+      content: [
+        {
+          link: 'https://www.prismaticcapital.com/',
+          heading: 'A cryptoasset investment firm',
+          text: 'View Work',
+          image: webflow1,
+        },
+      ],
+    },
     // Add more stages as needed
   ];
 
-  // Dynamically populate "All Works" tab content based on other tabs
-  stages[0].content = stages
+    // Dynamically populate "All Works" tab content based on other tabs
+    stages[0].content = stages
     .slice(1) // Exclude the "All Works" tab itself
     .flatMap((tab) => tab.content);
 
   const tabs = stages;
   const activeTab = activeTabStages;
+
+  // Calculate the index range for the current page
+  const startIndex = (currentPage - 1) * projectsPerPage;
+  const endIndex = startIndex + projectsPerPage;
 
   return (
     <div className="Section-1-Our-Work">
@@ -123,18 +170,37 @@ function ReactTabs() {
                 className={activeTab === tab.id ? 'show-content' : 'content'}
               >
                 {activeTab === tab.id &&
-                  tab.content.map((anchor, index) => (
+                  tab.content.slice(startIndex, endIndex).map((anchor, index) => (
                     <a key={index} href={anchor.link} className="content-anchor">
                       <img src={anchor.image} alt={`img-${index}`} />
                       <div className='content-dis'>
                         <p>{anchor.heading}</p>
-                        <span>{anchor.text} <div class="svg svg--arrow button-svg_arrow"><svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" class="svg-inline--fa fa-long-arrow-right fa-w-14" data-icon="long-arrow-right" data-prefix="fal" viewBox="0 0 448 512">
-                          <path fill="currentColor" d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z"></path>
-                        </svg>
-                        </div></span>
+                        <span>
+                          {anchor.text}{' '}
+                          <div class="svg svg--arrow button-svg_arrow">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              aria-hidden="true"
+                              class="svg-inline--fa fa-long-arrow-right fa-w-14"
+                              data-icon="long-arrow-right"
+                              data-prefix="fal"
+                              viewBox="0 0 448 512"
+                            >
+                              <path
+                                fill="currentColor"
+                                d="M311.03 131.515l-7.071 7.07c-4.686 4.686-4.686 12.284 0 16.971L387.887 239H12c-6.627 0-12 5.373-12 12v10c0 6.627 5.373 12 12 12h375.887l-83.928 83.444c-4.686 4.686-4.686 12.284 0 16.971l7.071 7.07c4.686 4.686 12.284 4.686 16.97 0l116.485-116c4.686-4.686 4.686-12.284 0-16.971L328 131.515c-4.686-4.687-12.284-4.687-16.97 0z"
+                              ></path>
+                            </svg>
+                          </div>
+                        </span>
                       </div>
                     </a>
                   ))}
+                {tab.content.length > endIndex && (
+                  <div className="show-more-button" onClick={handleShowMore}>
+                    Show More
+                  </div>
+                )}
               </div>
             ))}
           </div>
