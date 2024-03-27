85363fdab67056b71008b4154f50fdaab9cfdfb7
arsalankhan78677@gmail.com <you@example.com>
2024-03-28 02:32:05 +0500

    ss

--- src/Home.js
+++ src/Home.js
@@ -37,7 +37,7 @@ const Home = () => {
           <div className="left-sec-2">
             <div className="h4-sec-2">
               <p>
-                aFor a decade, Innovapixels has consistently delivered influential work,
+                For a decade, Innovapixels has consistently delivered influential work,
                 <strong>
                   empowering both Businesses and global influencers.
                 </strong>
